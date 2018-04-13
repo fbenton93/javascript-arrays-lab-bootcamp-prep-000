12d1f5e1361ce7c614b35a905948a4cb3a7973b4
Forrest Benton <elliottbenton@gmail.com>
2018-04-13 16:37:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(element) {
+  kittens.push(element)
+  return kittens
+}
+
+function destructivelyPrependKitten(element) {
+  kittens.unshift(element)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop() 
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name) {
+  moar_kittens = [...kittens,name]
+  return moar_kittens
+}
